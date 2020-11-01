c634d8c85ce104c2218e84a7ce7b994aa42cabe2
SYED MOHAMMAD MEHDI <48373772+SYEDMOHAMMADMEHDI@users.noreply.github.com>
2020-11-01 10:08:45 +0530

    Add files via upload

---
+++ StaticDemo.java
@@ -0,0 +1,27 @@
+package com.csscorp.keywords;
+
+public class StaticDemo {
+
+	public static int c;
+	static {
+		c=5;
+	}
+	private int a;
+	
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public StaticDemo() {
+		
+	}
+	public StaticDemo(int a) {
+		this.a=a;
+	}
+	public static void display() {
+		System.out.println("Display method (static)");
+	}
+}
+
